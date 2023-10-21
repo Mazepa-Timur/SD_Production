0b086bec0d8865d6ca8e0af00c9ecd263451d0e2
808`s Live <107718500+Mazepa-Timur@users.noreply.github.com>
2023-10-21 21:53:48 +0300

    Full main page

--- js/language.js
+++ js/language.js
@@ -1,10 +1,11 @@
-const listLanguage = document.querySelector('.langChange');
-
-listLanguage.addEventListener("click", ()=> {
+try {
+  const listLanguage = document.querySelector('.langChange');
+  listLanguage.addEventListener('click', () => {
     if (listLanguage.className === 'langChange langHide') {
-        listLanguage.className = 'langChange langShow';
+      listLanguage.className = 'langChange langShow';
     } else {
-        listLanguage.className = 'langChange langHide';
+      listLanguage.className = 'langChange langHide';
     }
     console.log(listLanguage.className);
-})
+  });
+} catch (error) {}
